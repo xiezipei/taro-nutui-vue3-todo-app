ba812ae2335ce3185363b532aacdaeb41bb29ddd
xiezipei <zipeixie@gmail.com>
2021-11-29 20:02:07 +0800

    update

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,25 +1,32 @@
 <template>
-  <h1>ToDo App</h1>
-  <div>
-    <label>New ToDo</label>
-    <input v-model="newTodo" name="newTodo" />
-    <button @click="addTodo()">Add ToDo</button>
+  <div class="app">
+    <div class="header">Todos</div>
+    <div>
+      <label>New ToDo</label>
+      <input v-model="newTodo" name="newTodo" />
+      <nut-input v-model="newTodo" :require-show="true" label="文本" />
+      <nut-button type="primary" @click="addTodo()">Add ToDo</nut-button>
+    </div>
+    <h2>ToDo List</h2>
+    <ul>
+      <li v-for="(todo, index) in todos" :key="index">
+        <span :class="{ done: todo.done }" @click="doneTodo(todo)">{{ todo.content }}</span>
+        <button @click="removeTodo(index)">Remove</button>
+      </li>
+    </ul>
+    <h4 v-if="todos.length === 0">Empty list.</h4>
   </div>
-  <h2>ToDo List</h2>
-  <ul>
-    <li v-for="(todo, index) in todos" :key="index">
-      <span :class="{ done: todo.done }" @click="doneTodo(todo)">{{ todo.content }}</span>
-      <button @click="removeTodo(index)">Remove</button>
-    </li>
-  </ul>
-  <h4 v-if="todos.length === 0">Empty list.</h4>
 </template>
 
 <script>
 import { ref } from 'vue';
 import Taro from '@tarojs/taro';
+import { Input } from '@nutui/nutui-taro';
 export default {
   name: 'App',
+  components: {
+    'nut-input': Input,
+  },
   setup() {
     const newTodo = ref('');
     const defaultData = [
@@ -28,13 +35,13 @@ export default {
         content: 'Write a blog post'
       }
     ]
-  
+
     const obj = Taro.getStorageSync('todos');  // => {}
-  console.log('todos', obj)
+    console.log('todos', obj)
 
 
     // const todosData = Object.keys(obj).length === 0 ? defaultData : obj;
-    
+
 
     const todosData = obj || defaultData;
 
@@ -56,6 +63,11 @@ export default {
       saveData();
     }
     function removeTodo(index) {
+      // Toast.text('删除成功');
+      Taro.showToast({
+        title: '删除成功',
+        icon: 'success'
+      })
       todos.value.splice(index, 1);
       saveData();
     }
@@ -80,105 +92,14 @@ export default {
 </script>
 
 <style lang="scss">
-$border: 2px solid
-  rgba(
-    $color: white,
-    $alpha: 0.35
-  );
-$size1: 6px;
-$size2: 12px;
-$size3: 18px;
-$size4: 24px;
-$size5: 48px;
-$backgroundColor: #27292d;
-$textColor: white;
-$primaryColor: #a0a4d9;
-$secondTextColor: #1f2023;
-body {
-  margin: 0;
-  padding: 0;
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  background-color: $backgroundColor;
-  color: $textColor;
-  #app {
-    max-width: 600px;
-    margin-left: auto;
-    margin-right: auto;
-    padding: 20px;
-    h1 {
-      font-weight: bold;
-      font-size: 28px;
-      text-align: center;
-    }
-    form {
-      display: flex;
-      flex-direction: column;
-      width: 100%;
-      label {
-        font-size: 14px;
-        font-weight: bold;
-      }
-      input,
-      button {
-        height: $size5;
-        box-shadow: none;
-        outline: none;
-        padding-left: $size2;
-        padding-right: $size2;
-        border-radius: $size1;
-        font-size: 18px;
-        margin-top: $size1;
-        margin-bottom: $size2;
-      }
-      input {
-        background-color: transparent;
-        border: $border;
-        color: inherit;
-      }
-    }
-    button {
-      cursor: pointer;
-      background-color: $primaryColor;
-      border: 1px solid $primaryColor;
-      color: $secondTextColor;
-      font-weight: bold;
-      outline: none;
-      border-radius: $size1;
-    }
-    h2 {
-      font-size: 22px;
-      border-bottom: $border;
-      padding-bottom: $size1;
-    }
-    ul {
-      padding: 10px;
-      li {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        border: $border;
-        padding: $size2 $size4;
-        border-radius: $size1;
-        margin-bottom: $size2;
-        span {
-          cursor: pointer;
-        }
-        .done {
-          text-decoration: line-through;
-        }
-        button {
-          font-size: $size2;
-          padding: $size1;
-        }
-      }
-    }
-    h4 {
-      text-align: center;
-      opacity: 0.5;
-      margin: 0;
-    }
+.app {
+  padding: 0 16px;
+  .header {
+    font-size: 32px;
+    font-weight: bold;
+  }
+  .done {
+    text-decoration: line-through;
   }
 }
 </style>
